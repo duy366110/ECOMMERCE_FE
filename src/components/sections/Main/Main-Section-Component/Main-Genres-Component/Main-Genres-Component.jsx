0e86ae39e04d47c7128d5d6ef88b888d37b997d3
duy366110 <duy366110@gmail.com>
2024-01-02 22:24:03 +0700

    #Commit 02/01/2023: COnfig feactor ecommerce cl.

--- src/components/sections/Main/Main-Section-Component/Main-Genres-Component/Main-Genres-Component.jsx
+++ src/components/sections/Main/Main-Section-Component/Main-Genres-Component/Main-Genres-Component.jsx
@@ -11,15 +11,25 @@ const MainGenresComponent = (props) => {
                 <h2 className="section-title text-center">Brpwse out categories</h2>
 
                 <div className="row">
-                    {props.categories.length > 0 && props.categories.map((category, index) => {
+                    {props.featureds.length > 0 && props.featureds.map((feature, index) => {
                         return (
-                            <div key={category._id} className="col-12 col-md-6">
+                            <div key={feature._id} className="col-12 col-md-6">
                                 <Link to="/shop">
                                     <div className={classes['card-genres']}>
-                                        <img src={category.images[0]} alt="category" />
+                                        <img src={feature.images[0]} alt="feature" />
                                         <div className={classes['card-genres-infor']}>
-                                            <h2 className={classes['card-genres-infor-title']}>{category.title}</h2>
-                                            <p className={classes['card-genres-infor-des']}>{category.desc}</p>
+                                            <h2 className={`
+                                                ${classes['card-genres-infor-title']}
+                                                ${feature.titleColor === "white"? classes['color-white'] : ''}
+                                            `}>
+                                                {feature.title}
+                                            </h2>
+                                            <p className={`
+                                                ${classes['card-genres-infor-des']}
+                                                ${feature.titleColor === "white"? classes['color-white'] : ''}
+                                            `}>
+                                                {feature.desc}
+                                            </p>
                                         </div>
                                     </div>
                                 </Link>
--- src/components/sections/Main/Main-Section-Component/Main-Section-Component.jsx
+++ src/components/sections/Main/Main-Section-Component/Main-Section-Component.jsx
@@ -1,37 +1,33 @@
-import React, { useEffect , useState } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import config from "../../../../configs/config.env";
 import MainBannerComponent from './Main-Banner-Component/Main-Banner-Component';
 import MainGenresComponent from './Main-Genres-Component/Main-Genres-Component';
-import CommonProductListComponent from '../../../common/Common-Product-List-Component/Common-Product-List-Component';
 import MainService from "./Main-Service-Component/Main-Service-Component";
 import classes from "./Main-Section-Component.module.css";
 
 const MainSectionComponent = (props) => {
     const loader  = useLoaderData();
-    const [categories, setCategories] = useState([]);
-    const [products, setProducts] = useState([]);
+    const [featureds, setFeatureds] = useState([]);
 
     // PHƯƠNG THỰC HIÊN MAP PRODUCT DATA.
-    const mapperProduct = () => {
-        let { status, message, products, categories, error} = loader;
+    const mapperProduct = useCallback(() => {
+        let { status, featureds} = loader;
 
         if(status) {
-            setProducts(products);
-            setCategories(categories);
+            console.log(featureds);
+            setFeatureds(featureds);
         }
-    }
+    }, [loader])
 
     useEffect(() => {
         mapperProduct();
-
-    }, [])
+    }, [mapperProduct])
 
     return (
         <div className={classes['main-component']}>
             <MainBannerComponent />
-            <MainGenresComponent categories={categories}/>
-            {/* <CommonProductListComponent products={products} hasTitle={true}/> */}
+            <MainGenresComponent featureds={featureds}/>
             <MainService />
         </div>
     )
@@ -39,12 +35,12 @@ const MainSectionComponent = (props) => {
 
 export default MainSectionComponent;
 
-// LOADER CATEGORY
-const loaderCategory = () => {
+// LOADER FEATURED
+const loaderFeatured = () => {
     return new Promise(async (resolve, reject) => {
         try {
 
-            let res = await fetch(`${config.URI}/api/client/category`, {
+            let res = await fetch(`${config.URI}/api/client/featured`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
@@ -94,9 +90,9 @@ export const loader = (request, params) => {
     return new Promise( async(resolve, reject) => {
         try {
 
-            let data = await Promise.all([loaderProduct(), loaderCategory()]);
-            let [{products}, {categories}] = data;
-            resolve({ status: true , products, categories });
+            let data = await Promise.all([loaderProduct(), loaderFeatured()]);
+            let [{products}, {featureds}] = data;
+            resolve({ status: true , products, featureds });
 
         } catch (error) {
             reject(error);
