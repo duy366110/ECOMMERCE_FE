3144e83fe918cd6ac7d287b397471a9e7d734987
duy366110 <duy366110@gmail.com>
2024-01-03 16:19:17 +0700

    #COmmit 03/01/2024: Config render product to product list page.

--- src/store/store.serach.jsx
+++ src/store/store.serach.jsx
@@ -4,15 +4,20 @@ const initState = {
     type: "all",
     itemPage: 5,
     elemtItemsPagination: 0,
-    currentPage: 0
+    currentPage: 0,
+    amountProductOfType: 0
 }
 
 const searchSlice = createSlice({
     name: "Search_slice",
     initialState: initState,
     reducers: {
-        
+        loaderInforSearch: (state, action) => {
+            state.amountProductOfType = action.payload.amount;
+            state.elemtItemsPagination = Math.ceil(action.payload.amount / state.itemPage);
+        }
     }
 });
 
+export const { loaderInforSearch } = searchSlice.actions;
 export default searchSlice.reducer;
